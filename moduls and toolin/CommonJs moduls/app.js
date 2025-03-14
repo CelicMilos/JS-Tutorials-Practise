19f0bd32e679c482e9fafd5b0acad8881a91c438
CelicMilos <165153757+CelicMilos@users.noreply.github.com>
2025-03-14 22:02:37 +0100

    Add files via upload

--- moduls and toolin/CommonJs moduls/app.js
+++ moduls and toolin/CommonJs moduls/app.js
@@ -1,1 +1,9 @@
+const message = require("./utils.js");
+const Person = require("./Person");
+console.log(message);
 
+const { capitalizedWords, moneySign } = require("./utils.js");
+console.log(capitalizedWords("show me the money"));
+console.log(moneySign(1223));
+const person1 = new Person("Clam");
+person1.greet();
